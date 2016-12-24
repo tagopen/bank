9bd65af2746317cbefcdb1f2c8a51030ca5dd8f1
marchik88 <marchik88@rambler.ru>
2016-12-24 02:01:34 +0200

    button show all

--- app/js/main.js
+++ app/js/main.js
@@ -50,3 +50,26 @@ $(window).on('resize', function(){
     $('.product__text').equialHeight();
   }
 }).trigger('resize');
+
+$(window).on('resize', function(){
+  if( $( window ).width() <=967 ) {
+    $(document).ready( function () {
+      $('.service__item').slice(4).hide();
+      $(document).on("click", ".service__show-btn", (function(e) {
+        e.preventDefault();
+        $('.service__item').show();
+        $('.service__show-btn').hide();
+      }));
+    });
+  }
+  if( $( window ).width() < 1200) {    /*Не знаю корректно ли задал это условие , и возможно, можно было код короче сделать, тут уже если надо переделай и да, там еще перебей в бутстрапе колонки, (надо что бы при 960-1199 уже было md, а оно по прежнему срабатывает при 992+)*/
+    $(document).ready( function () {
+      $('.service__item').slice(6).hide();
+      $(document).on("click", ".service__show-btn", (function(e) {
+        e.preventDefault();
+        $('.service__item').show();
+        $('.service__show-btn').hide();
+      }));
+    });
+  }
+}).trigger('resize');
